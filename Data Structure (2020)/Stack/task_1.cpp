27acc1c7401b098adff39fcecda7fbd92b291830
Jason <xjasonlyu@gmail.com>
2020-04-07 17:38:29 +0800

    reformated code

--- Data Structure (2020)/Stack/task_1.cpp
+++ Data Structure (2020)/Stack/task_1.cpp
@@ -4,60 +4,65 @@ using namespace std;
 #define STACKSIZE 10
 typedef int DataType;
 
-typedef struct {
-	DataType* base;
-	DataType* top;
-	int	stacksize;
-}SqStack;
+typedef struct
+{
+	DataType *base;
+	DataType *top;
+	int stacksize;
+} SqStack;
 
 //初始化空顺序栈
-int InitStack(SqStack& S) {
-    S.base = new DataType[STACKSIZE];
-    if (S.base == NULL)
-        return 0;
-    // init
-    S.top = S.base;
-    S.stacksize = STACKSIZE;
-    return 1;
+int InitStack(SqStack &S)
+{
+	S.base = new DataType[STACKSIZE];
+	if (S.base == NULL)
+		return 0;
+	// init
+	S.top = S.base;
+	S.stacksize = STACKSIZE;
+	return 1;
 }
 
 //判栈空
-int StackEmpty(SqStack S) {
-    return (S.base == S.top) ? 1 : 0;
+int StackEmpty(SqStack S)
+{
+	return (S.base == S.top) ? 1 : 0;
 }
 
 //入栈
-int Push(SqStack& S, DataType item) {
-    if (S.top - S.base == S.stacksize)
-        return 0;
-    *(S.top++) = item;
-    return 1;
+int Push(SqStack &S, DataType item)
+{
+	if (S.top - S.base == S.stacksize)
+		return 0;
+	*(S.top++) = item;
+	return 1;
 }
 
-
 //出栈
-int Pop(SqStack& S, DataType& item) {
-    if (S.top - S.base == 0)
-        return 0;
-    item = *(--S.top);
-    return 1;
+int Pop(SqStack &S, DataType &item)
+{
+	if (S.top - S.base == 0)
+		return 0;
+	item = *(--S.top);
+	return 1;
 }
 
 void conversion(int N)
-{//对于任意一个非负十进制数，打印输出与其等值的八进制数
+{ //对于任意一个非负十进制数，打印输出与其等值的八进制数
 	SqStack S;
-	InitStack(S);	//初始化空栈S
-	while(N) {//当N非零时,循环
-		Push(S,N%8);	//把N与8求余得到的八进制数压入栈S
-		N=N/8;		//N更新为N与8的商
-   }
+	InitStack(S); //初始化空栈S
+	while (N)
+	{					//当N非零时,循环
+		Push(S, N % 8); //把N与8求余得到的八进制数压入栈S
+		N = N / 8;		//N更新为N与8的商
+	}
 	int e;
 	cout << "转化的八进制数为：";
-	while(!StackEmpty(S))//当栈S非空时，循环
+	while (!StackEmpty(S)) //当栈S非空时，循环
 	{
-		Pop(S,e);		//弹出栈顶元素e
-		cout<<e;		//输出e
-   }
+		Pop(S, e); //弹出栈顶元素e
+		cout << e; //输出e
+	}
 }
 
 int main()
--- Data Structure (2020)/Stack/task_2.cpp
+++ Data Structure (2020)/Stack/task_2.cpp
@@ -6,14 +6,16 @@ using namespace std;
 #define STACKSIZE 100
 typedef char DataType;
 
-typedef struct {
-	DataType* base;
-	DataType* top;
-	int	stacksize;
-}SqStack;
+typedef struct
+{
+    DataType *base;
+    DataType *top;
+    int stacksize;
+} SqStack;
 
 //初始化空顺序栈
-int InitStack(SqStack& S) {
+int InitStack(SqStack &S)
+{
     S.base = new DataType[STACKSIZE];
     if (S.base == NULL)
         return 0;
@@ -24,21 +26,23 @@ int InitStack(SqStack& S) {
 }
 
 //判栈空
-int StackEmpty(SqStack S) {
+int StackEmpty(SqStack S)
+{
     return (S.base == S.top) ? 1 : 0;
 }
 
 //入栈
-int Push(SqStack& S, DataType item) {
+int Push(SqStack &S, DataType item)
+{
     if (S.top - S.base == S.stacksize)
         return 0;
     *(S.top++) = item;
     return 1;
 }
 
-
 //出栈
-int Pop(SqStack& S, DataType& item) {
+int Pop(SqStack &S, DataType &item)
+{
     if (S.top - S.base == 0)
         return 0;
     item = *(--S.top);
@@ -46,13 +50,15 @@ int Pop(SqStack& S, DataType& item) {
 }
 
 //取栈顶，细节在此不表
-int GetTop(SqStack S, DataType &item) {
-    item = *(S.top-1);
+int GetTop(SqStack S, DataType &item)
+{
+    item = *(S.top - 1);
     return 1;
 }
 
 //表达式括号匹配判断函数，自定义
-int matching(char* exp) {
+int matching(char *exp)
+{
     char c;
     char w;
 
@@ -61,7 +67,8 @@ int matching(char* exp) {
     SqStack L;
     InitStack(L);
 
-    for (int i=0; i < strlen(exp); i++) {
+    for (int i = 0; i < strlen(exp); i++)
+    {
         c = exp[i];
         switch (c)
         {
@@ -107,15 +114,16 @@ out:
     return 0;
 }
 
-int main() {
-	char expr[100];
-	//cout << "请输入表达式:";
-	cin >> expr;
-	char* ex = expr;
-	if (matching(ex))
+int main()
+{
+    char expr[100];
+    //cout << "请输入表达式:";
+    cin >> expr;
+    char *ex = expr;
+    if (matching(ex))
         cout << "匹配成功";
-	else
+    else
         cout << "匹配不成功";
 
-	return 0;
+    return 0;
 }
