bc593c5de9b3e51576857cef67a5b35a0ed23eea
Jason <xjasonlyu@gmail.com>
2020-05-08 14:06:27 +0800

    reformat verilog

--- Basis of Modern Digital Electronics Technology (2019)/Digital Alarm/div_1kHz.v
+++ Basis of Modern Digital Electronics Technology (2019)/Digital Alarm/div_1kHz.v
@@ -1,24 +1,26 @@
-module div_1kHz(clk_out, clk, rst_n);
-
-	input clk, rst_n;
-	output clk_out;
-
-	reg clk_out;
-	reg [31:0] cnt;
-
-always @ (posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-        cnt     <= 5'b0;
-        clk_out <= 1'b0;
+module div_1kHz(clk_out,
+                clk,
+                rst_n);
+    
+    input clk, rst_n;
+    output clk_out;
+    
+    reg clk_out;
+    reg [31:0] cnt;
+    
+    always @ (posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            cnt     <= 5'b0;
+            clk_out <= 1'b0;
+        end
+        else if (cnt < 32'd24999) begin
+            cnt     <= cnt + 1'b1;
+            clk_out <= clk_out;
+        end
+        else begin
+            cnt     <= 5'b0;
+            clk_out <= ~clk_out;
+        end
     end
-    else if(cnt < 32'd24999) begin
-        cnt     <= cnt + 1'b1;
-        clk_out <= clk_out;
-    end
-    else begin
-        cnt     <= 5'b0;
-        clk_out <= ~clk_out;
-    end
-end
-
+    
 endmodule
