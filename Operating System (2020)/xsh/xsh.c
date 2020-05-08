da759dffd34c4d6c4eea394a35c6af2b24c76c3b
Jason <xjasonlyu@gmail.com>
2020-05-08 11:01:49 +0800

    xsh: add comment

--- Operating System (2020)/xsh/xsh.c
+++ Operating System (2020)/xsh/xsh.c
@@ -181,7 +181,7 @@ int main()
     if (init_shell() < 0)
         die(EXIT_FAILURE, "Init shell failed");
 
-    while (1)
+    while (1) /* infinite loop */
     {
         show_promot(last_retval); /* print shell promot */
 
